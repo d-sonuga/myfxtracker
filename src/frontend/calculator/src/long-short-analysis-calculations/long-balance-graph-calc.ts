ef4b66abab7168626fdf8cb010f798c4d4577cdb
Demilade <sonugademilade8703@gmail.com>
2022-02-24 14:15:50 +0100

    made long balance graph cummulative

--- src/frontend/calculator/src/long-short-analysis-calculations/long-balance-graph-calc.ts
+++ src/frontend/calculator/src/long-short-analysis-calculations/long-balance-graph-calc.ts
@@ -7,13 +7,18 @@ import {LongBalanceGraphCalc} from './types'
  * number 1 implies that it was carried out before a trade with an index number 2
  */
 const longBalanceGraphCalc = (accountData: AccountData) => {
+    let cummulativeResult = 0;
     const calculations: LongBalanceGraphCalc = [
         {tradeNo: 0, result: 0},
         ...accountData.trades
             .filter((trade) => trade.action === 'buy')
-            .map((trade, i) => (
-                {tradeNo: i + 1, result: trade.profitLoss}
-            ))
+            .map((trade, i) => {
+                cummulativeResult += trade.profitLoss;
+                return {
+                    tradeNo: i + 1,
+                    result: cummulativeResult
+                }
+            })
     ]
     return calculations
 }
