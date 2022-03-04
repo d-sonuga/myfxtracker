789543b6cb1a9ea2fcd0eff1fdc701dd5cae760f
Demilade <sonugademilade8703@gmail.com>
2022-03-04 11:10:00 +0100

    checking if this analytics will work

--- src/frontend/src/apps/info-app/pages/home/index.tsx
+++ src/frontend/src/apps/info-app/pages/home/index.tsx
@@ -8,9 +8,14 @@ import WhyTradersPreferUsSection from './why-traders-prefer-us-section'
 import ReviewSection from './review-section'
 import SignUpOfferingSection from './sign-up-offering-section'
 import Footer from './footer'
+import { useEffect } from 'react'
+import ReactGA from 'react-ga4'
 
 
 const HomePage = () => {
+    useEffect(() => {
+        ReactGA.send('pageview')
+    })
     return(
         <div>
             <InfoAppNavbar />
