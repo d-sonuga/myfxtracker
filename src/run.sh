c4b825968b785e32abad03c2677e4a3f8116303e
Demilade <sonugademilade8703@gmail.com>
2022-04-03 22:04:19 +0100

    resolving requirements

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,5 @@
 echo 'Running Myfxtracker'
 cd backend
-PATH="$PATH:~/.local/bin/"
-pipenv run python manage.py collectstatic --noinput
-pipenv run python manage.py migrate
-pipenv run daphne core.asgi:application -b 0.0.0.0 -p $PORT
+python manage.py collectstatic --noinput
+python manage.py migrate
+daphne core.asgi:application -b 0.0.0.0 -p $PORT
