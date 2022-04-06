0393ae6cd3aad63b2cb05f8f331b1679c9b7f95a
Demilade <sonugademilade8703@gmail.com>
2022-04-06 16:55:29 +0100

    added stuff to Dockerfile to support local bash sessions in container

--- src/run.sh
+++ src/run.sh
@@ -3,4 +3,5 @@ cd backend
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py rqworker default low &
+rm /bin/sh && ln -s /bin/bash /bin/sh
 daphne core.asgi:application -b 0.0.0.0 -p $PORT
