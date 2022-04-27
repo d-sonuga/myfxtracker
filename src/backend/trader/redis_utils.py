69820ece136a788f0b7f41e892344b8d8b3302ad
Demilade <sonugademilade8703@gmail.com>
2022-04-27 17:13:16 +0100

    attempt to restrict redis connections

--- src/backend/trader/redis_utils.py
+++ src/backend/trader/redis_utils.py
@@ -1,14 +1,16 @@
 import logging
 import django_rq
+from redis import StrictRedis
+from django.conf import settings
 
-logger = logging.getLogger()
-low_class_conn = django_rq.get_queue('low')
-default_class_conn = django_rq.get_queue('default')
+logger = logging.getLogger(__name__)
+default_class_conn = StrictRedis.from_url(settings.RQ_QUEUES['default']['url'])
+low_class_conn = StrictRedis.from_url(settings.RQ_QUEUES['low']['url'])
 
 def rq_enqueue(*args, **kwargs):
     if kwargs.get('queue_class', 'default') == 'default':
         logger.info('Enqueueing job on the default queue')
-        default_class_conn.enqueue(*args, **kwargs)
+        django_rq.get_queue('low', connection=low_class_conn).enqueue(*args, **kwargs)
     else:
         logger.info('Enqueueing job on the low queue')
-        low_class_conn.enqueue(*args, **kwargs)
+        django_rq.get_queue('default', connection=default_class_conn).enqueue(*args, **kwargs)
