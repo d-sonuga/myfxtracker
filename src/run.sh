b4579b25ed89c2bbf0c3639fcd0209ce39ca2599
Demilade <sonugademilade8703@gmail.com>
2022-04-27 16:45:39 +0100

    attempt to flush redis db on start

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,6 @@ echo 'Running Myfxtracker'
 cd backend
 python manage.py collectstatic --noinput
 python manage.py migrate
-daphne core.asgi:application -b 0.0.0.0 -p $PORT &
 python manage.py rqworker default low &
 python manage.py rqscheduler &
+daphne core.asgi:application -b 0.0.0.0 -p $PORT &
