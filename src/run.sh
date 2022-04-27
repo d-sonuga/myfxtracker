301c7e0ee37369a202b3041a457cfe5868cddeb5
Demilade <sonugademilade8703@gmail.com>
2022-04-27 16:22:35 +0100

    attempt to remove rq logging temporarily

--- src/run.sh
+++ src/run.sh
@@ -2,6 +2,7 @@ echo 'Running Myfxtracker'
 cd backend
 python manage.py collectstatic --noinput
 python manage.py migrate
+export DJANGO_SETTINGS_MODULE="core/settings.py"
 python trader/clear_redis_db.py
 python manage.py rqworker default low &
 python manage.py rqscheduler &
