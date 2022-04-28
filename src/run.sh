25597e1b57e30677f47ad4d10bd1f3d0dad7ede9
Demilade <sonugademilade8703@gmail.com>
2022-04-28 23:00:03 +0100

    attempting to schedule before starting the server

--- src/run.sh
+++ src/run.sh
@@ -5,4 +5,5 @@ python manage.py migrate
 python manage.py rqworker default low &
 python manage.py rqscheduler &
 python trader/setup_scheduler.py
+echo 'Starting server'
 daphne core.asgi:application -b 0.0.0.0 -p $PORT
