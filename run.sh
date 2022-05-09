1a50d8f82753d7b1be66f98a2d5fd9f456452f95
Demilade <sonugademilade8703@gmail.com>
2022-05-09 13:07:19 +0100

    accounts refresh periodically with heroku worker attempt

--- run.sh
+++ run.sh
@@ -2,8 +2,5 @@ echo 'Running Myfxtracker'
 cd backend
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py rqworker default low &
-python manage.py rqscheduler &
-python trader/setup_scheduler.py
 echo 'Starting server'
 daphne core.asgi:application -b 0.0.0.0 -p $PORT
