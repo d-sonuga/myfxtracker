1f99caf187a6dc3a619934edc743182f721567ae
demilade <sonugademilade8703@gmail.com>
2022-08-01 09:00:47 +0100

    Registered models for admin site

--- src/backend/trader/admin.py
+++ src/backend/trader/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import (Account, Trade, Deposit, Withdrawal,
+    UnknownTransaction, FailedTransactionSave, MetaApiError, AccountDataLastRefreshed)
 
-# Register your models here.
+admin.site.register(Account)
+admin.site.register(Trade)
+admin.site.register(Deposit)
+admin.site.register(Withdrawal)
+admin.site.register(UnknownTransaction)
+admin.site.register(FailedTransactionSave)
+admin.site.register(MetaApiError)
+admin.site.register(AccountDataLastRefreshed)
--- src/backend/users/admin.py
+++ src/backend/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, Trader, SubscriptionInfo
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Trader)
+admin.site.register(SubscriptionInfo)
