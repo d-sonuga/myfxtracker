5d11660d1350d94e5dca296d7420b791f3032cb9
demilade <sonugademilade8703@gmail.com>
2022-08-24 20:50:45 +0100

    added test for affiliate init data

---
+++ src/backend/affiliate/tests/test_init_data.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+from rest_framework.authtoken.models import Token
+from test_data.login_test_data import LoginTestData
+from trader.tests.test_data import SignUpDetails
+from users.models import Affiliate, Trader, SubscriptionInfo
+
+
+class InitDataTests(TestCase):
+    def setUp(self) -> None:
+        self.affiliate = Affiliate.objects.create_affiliate(**LoginTestData.affiliate_details)
+        self.trader = Trader.objects.create_trader(**SignUpDetails.good_details)
+        self.affiliate_token = Token.objects.create(user=self.affiliate)
+        self.trader_token = Token.objects.create(user=self.trader)
+
+    def test_init_data_valid_token_no_referred_users(self):
+        """
+        To test the scenario where an init data request is made with a
+        token belonging to a valid affiliate with no referred users
+        """
+        resp = self.make_request(self.affiliate_token)
+        self.assertEquals(resp.status_code, 200)
+        referred_users_subscription_info = SubscriptionInfo.objects.filter(referrer=self.affiliate)
+        referred_users = [si.user for si in referred_users_subscription_info]
+        self.assertEqual(resp.json(), {
+            'username': self.affiliate.user.username,
+            'no_of_sign_ups': len(referred_users),
+            'no_of_subscribers': referred_users_subscription_info.filter(is_subscribed=True),
+            'bank_account_number': self.affiliate.bank_account_number
+        })
+    
+    def test_init_data_valid_token_some_referred_users(self):
+        """
+        To test the scenario where an init data request is made with a
+        token belonging to a valid affiliate with some referred users
+        """
+    
+    def test_init_data_non_affiliate_token(self):
+        """
+        To test the scenario where an init data request is made with a token
+        belonging to a non affiliate
+        """
+
+    def test_init_data_invalid_token(self):
+        """
+        To test the scenario where an init data request is made with
+        an invalid token
+        """
