7c73e603ca29a102576eb8ac66abecc4811f5216
demilade <sonugademilade8703@gmail.com>
2022-08-26 10:32:33 +0100

    affiliate can now be modified on the admin dashboard

--- src/backend/users/admin.py
+++ src/backend/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import User, Trader, SubscriptionInfo
+from .models import User, Trader, SubscriptionInfo, Affiliate
 
 admin.site.register(User)
 admin.site.register(Trader)
 admin.site.register(SubscriptionInfo)
+admin.site.register(Affiliate)
