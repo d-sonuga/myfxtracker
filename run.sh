e9ab888f37ddb1e5639cd591970303abf3fe0a56
demilade <sonugademilade8703@gmail.com>
2023-12-29 22:50:42 +0100

    added creation of dummy user to the run.sh file

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ echo 'Running Myfxtracker'
 cd backend
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py seed
 echo 'Starting server'
 daphne core.asgi:application -b 0.0.0.0 -p $PORT
